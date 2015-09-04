b97151da8578047251fb5163aef1da4eb7120b31
Nathan Leonard <n.leonard668@gmail.com>
2015-09-04 10:55:40 -0500

    Completed up to showing discount

--- src/components/coupon/coupon.js
+++ src/components/coupon/coupon.js
@@ -1,11 +1,39 @@
 
 Coupon = {}
+// Callback-style component
 
-Coupon.controller = function () {
+Coupon.controller = function (options) {
   var ctrl = this;
-  // Controller code goes here
+  ctrl.code = m.prop('');
+  ctrl.error = m.prop(null);
+
+  ctrl.submit = function (e) {
+    e.preventDefault();
+    ctrl.error(null); //reset to null before each submit
+    validateCoupon(ctrl.code())
+      .then(options.onSuccess, ctrl.error) // <-- this makes it callback-style
+  }
 }
 
 Coupon.view = function (ctrl) {
-  // View code goes here
+  return m('form', { onsubmit: ctrl.submit }, [
+    ctrl.error() ? [
+      m('.error', "Invalid coupon.")
+    ] : null,
+    m('label', 'Enter coupon (if you have one):'),
+    m('input[type=text]', {
+      value: ctrl.code(),
+      onchange: m.withAttr('value', ctrl.code)
+    }),
+    m('button[type=submit]', 'Validate coupon')
+  ])
+}
+
+function validateCoupon (code) {
+  var isValid = (code === 'happy') // This should be done on the server
+  var discount = 0.20;  // And this
+  var deferred = m.deferred() // Mock AJAX call
+  if ( isValid ) { deferred.resolve(discount) }
+  else           { deferred.reject('invalid_code') }
+  return deferred.promise;
 }
--- src/components/total/total.js
+++ src/components/total/total.js
@@ -1,6 +1,20 @@
 
 Total = {}
+// This is a stateless component (no controller)
 
-Total.view = function () {
-  // View code goes here
-}
+Total.pricePerCount = 10;
+Total.calcPrice = function (discount, count) {
+  var total = count * Total.pricePerCount;
+  return roundCents(total - total * discount);
+};
+
+Total.view = function (ctrl, options) {
+  return m('.total', [
+    m('label', 'Total: '),
+    m('b', "$" + Total.calcPrice(options.discount, options.count))
+  ]);
+};
+
+function roundCents (num) {
+  return Math.round(num * 100) / 100;
+};
